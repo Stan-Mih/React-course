343cc46fd0c32a58eb5a6a6b11699c89879e06f6
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-07-25 15:28:10 +0300

    exercise

--- 01create components/index.js
+++ 01create components/index.js
@@ -12,11 +12,12 @@ class Hello extends React.Component {
 
 // function Hello(){
 // 	return(
-// 		<div>							// must return only one element, so wrap thing in a "div"
+// 		<div>							// must return only one element, so wrap multiple in a "div"
 // 			<h1>Hello there!</h1>	
 //			<h1>Hello there!</h1>	
 // 		</div>
 // 	);
 // }
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Hello />, document.getElementById('root'));   // render the component in HTML
+ 
