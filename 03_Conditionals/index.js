9ce911933910fbe24788775544488b50131f1704
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-07-26 09:40:14 +0300

    exercise

--- 03_Conditionals/index.js
+++ 03_Conditionals/index.js
@@ -1,34 +1,32 @@
 class WhatPuppyDoes extends React.Component {
 	render() {
-        const actions = puppyActions();
-        const puppyPic = "";
-        switch (actions) {
-                    case "bark":
-                        puppyPic = "GoodBoyBarks.jpg"
-                        break;
-                    case "eat":
-                        puppyPic = "GoodBoyEats.jpg"
-                        break;
-                    case "poop":
-                        puppyPic = "GoodBoyPoops.jpg"
-                        break;
-                    case "vomit":
-                        puppyPic = "GoodBoyVomit.jpg"
-                        break;
-                    case "retrieve":
-                        puppyPic = "GoodBoyRetreves.jpg"
-                        break;
-                
-                    default:
-                        break;
-                }
-		return (	
+        const actions = puppyActions();   
+        const pic =   gettingImg(actions);
+       
+		// return (	
+        //     <div>	
+        //         <h1>Your puppy will: {actions}!</h1>	
+        //         <p>{actions === "retrieve" 
+        //             ? "Good Boy" 
+        //             : "Lazy boy"}
+        //         </p>
+        //         {
+        //             actions === "retrieve" &&
+        //             <img src = "PuppyRetreves.jpg" />                    
+        //         }
+        //         {
+        //             actions === "bark" &&
+        //             <img src = "PuppyBarks.jpg" />
+        //         }
+        //     </div>	
+		// );
+
+        return (
             <div>	
                 <h1>Your puppy will: {actions}!</h1>	
-                <img src={puppyPic} />
-                
-            </div>	
-		);
+                <img src = {pic} />
+            </div>
+        )
 	}
 }   	
 function puppyActions(){
@@ -36,7 +34,31 @@ function puppyActions(){
     const action = thingsDogDo[Math.floor(Math.random()*thingsDogDo.length)];
     return action;
 }
-
+function gettingImg(input){
+    let puppyPic = "";
+    switch (input) {
+        case "bark":
+            puppyPic = "PuppyBarks.jpg"
+            break;
+        case "eat":
+            puppyPic = "PuppyEats.jpg"
+            break;
+        case "poop":
+            puppyPic = "PuppyPoops.jpg"
+            break;
+        case "vomit":
+            puppyPic = "PuppyVomit.jpg"
+            break;
+        case "retrieve":
+            puppyPic = "PuppyRetreves.jpg"
+            break;
+    
+        default:
+            break;
+    }
+    return puppyPic;
+}
+   
 
 
 ReactDOM.render(<WhatPuppyDoes />, document.getElementById('root'));
