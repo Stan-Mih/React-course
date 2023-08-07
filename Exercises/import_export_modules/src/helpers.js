927b0637747dc6dc681cf4b4fd56e012430637bb
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-08-07 10:37:44 +0300

    exercise

---
+++ Exercises/import_export_modules/src/helpers.js
@@ -0,0 +1,12 @@
+function helpful() {
+    console.log('I did a Super hlepful thing'); // this function shuld actualy making something helpful
+}
+function sort(){
+    console.log('Stuff is sorted');
+}
+function sing(){
+    console.log('LA-LA-La');
+}
+//export default helpful; // defult is used for exporting the "most likely thing"
+
+export{helpful, sort, sing}
