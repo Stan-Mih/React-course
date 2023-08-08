d2be4d9affb37b9c9d1ecc596db46ac1aec5474d
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-08-08 10:29:16 +0300

    exercise

---
+++ 01.8_Css__And_Assets/css_adn_assets/src/Dog.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Dog.css';
+import pitbull from './cuteDog.jpg';
+class Dog extends Component {
+    render(){
+        return(
+            <div className = "Dog">                
+                <h1>DOG!</h1>
+                <img className = "Dog-picture" src = {pitbull}/>
+                <div>ANOTER DIV, BUT NOT IN DOG COMPONENT</div>               
+            </div>
+        )
+    }
+}
+export default Dog;
