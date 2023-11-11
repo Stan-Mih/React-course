d038e7039a1e25a0e886694ca611eba91ac07233
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-11-11 16:01:45 +0200

    tasks update

--- Exercises/4_Pokemon2/pokemon/src/Pokecard.js
+++ Exercises/4_Pokemon2/pokemon/src/Pokecard.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import "./Pokecard.css"
+
 const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/"
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3): number);   // padding the id number to three digits 
+
 class Pokecard extends Component{
     render() {
-        let imgSrc = `${POKE_API}${this.props.id}.png`;
+        let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
         return(
             <div className='Pokecard'>  
-                <h1>{this.props.name}</h1>
+                <h1 className="Pockecard-tittle">{this.props.name}</h1>
+                <div className='Pokecard-img-frame'>
+
                 <img className="Pokecard-img"src={imgSrc} alt={this.props.name}/>
-                <div>Type:{this.props.type}</div>
-                <div>XP:{this.props.base_experience}</div>
+                </div>
+                <div className="Pokecard-data">Type:{this.props.type}</div>
+                <div className="Pokecard-data">XP:{this.props.exp}</div>
             </div>
         )
     }
