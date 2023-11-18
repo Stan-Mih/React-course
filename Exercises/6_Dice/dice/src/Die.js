75a246a4b878b5e74039670e7f3edf8c99790590
Stnislav Mihaylov <109460661+Stan-Mih@users.noreply.github.com>
2023-11-18 09:38:45 +0200

    task update

--- Exercises/6_Dice/dice/src/Die.js
+++ Exercises/6_Dice/dice/src/Die.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDiceOne } from '@fortawesome/free-solid-svg-icons';
 import { faDiceTwo } from '@fortawesome/free-solid-svg-icons';
@@ -6,14 +6,12 @@ import { faDiceThree } from '@fortawesome/free-solid-svg-icons';
 import { faDiceFour } from '@fortawesome/free-solid-svg-icons';
 import { faDiceFive } from '@fortawesome/free-solid-svg-icons';
 import { faDiceSix } from '@fortawesome/free-solid-svg-icons';
+import "./Die.css"
 
-
-class Die extends Component{
-    render(){       
-        return(            
-            <div>                                
-                <FontAwesomeIcon icon={this.props.num} />                  
-            </div>
+class Die extends Component {
+    render() {
+        return (
+            <FontAwesomeIcon className={`Die ${this.props.rolling && "Shaking"}`}  icon={this.props.num}  />
         )
     }
 }
--- Exercises/6_Dice/dice/src/RollDice.js
+++ Exercises/6_Dice/dice/src/RollDice.js
@@ -1,6 +1,7 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Die from "./Die";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './RollDice.css'
+
 import { faDiceOne } from '@fortawesome/free-solid-svg-icons';
 import { faDiceTwo } from '@fortawesome/free-solid-svg-icons';
 import { faDiceThree } from '@fortawesome/free-solid-svg-icons';
@@ -8,35 +9,42 @@ import { faDiceFour } from '@fortawesome/free-solid-svg-icons';
 import { faDiceFive } from '@fortawesome/free-solid-svg-icons';
 import { faDiceSix } from '@fortawesome/free-solid-svg-icons';
 
-function die(){
-    const dies = [faDiceOne,faDiceTwo,faDiceThree,faDiceFour,faDiceFive,faDiceSix];
-    const die = dies[Math.floor(Math.random()*dies.length)];
-    return die;
-}
-class RollDice extends Component{
-    
-    constructor(props){
-        super(props);
-        this.state = {num: faDiceOne};                   
-        this.click = this.click.bind(this);   
-       }
-       click(e) {
-        const dies = [faDiceOne,faDiceTwo,faDiceThree,faDiceFour,faDiceFive,faDiceSix];
-        let die = dies[Math.floor(Math.random()*dies.length)];    
-        this.setState ({num : die});              
-        }
 
-    render(){
-        return(
-            <div>
-                <Die num={die()}/>    
-                <Die num={die()}/>   
-                <button onClick={this.click}>Roll Dice!</button>          
+class RollDice extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { num1: faDiceOne, num2: faDiceOne, rolling: false };
+        this.click = this.click.bind(this);
+    }
+    click(e) { // pick two new rolls
+        const dies = [faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix];
+        const die1 = dies[Math.floor(Math.random() * dies.length)];
+        const die2 = dies[Math.floor(Math.random() * dies.length)];
+        this.setState({ num1: die1, num2: die2, rolling: true }); // set state with new rolls. When we change state the the die is rerendering
+
+        //wait 1 sec then set rollin to false  
+        setTimeout(() => {
+            this.setState({ rolling: false });
+        }, 1000);
+    }
+
+
+    render() {
+        return (
+            <div className="RollDice">
+                <div>
+                    <Die num={this.state.num1} rolling={this.state.rolling} />
+                    <Die num={this.state.num2} rolling={this.state.rolling} />
+                </div>
+                <div>
+                    <button onClick={this.click} disabled={this.state.rolling}> {/* disabling the button while state rolling is true*/}
+                        {this.state.rolling ? "Rolling..." : "Roll Dice!"}
+                    </button>
+                </div>
+
             </div>
         )
     }
 }
 export default RollDice;
-
-
- 
